8616866d861c226a5e44bd3ca8877538531ab9db
Martin33 <martintroncoso43@gmail.com>
2023-12-08 14:27:24 -0300

    primerCommit

--- src/components/MGalery/MGalery.jsx
+++ src/components/MGalery/MGalery.jsx
@@ -1,7 +1,7 @@
 //PNG MOUSTRITOS GALERY
 import M1Galery from "./moustrosGalery/M1Galery.png"
-import M2Galery from "./moustrosGalery/M2Galery.png"
-import M3Galery from "./moustrosGalery/M3Galery.png"
+//import M2Galery from "./moustrosGalery/M2Galery.png"
+//import M3Galery from "./moustrosGalery/M3Galery.png"
 import M4Galery from "./moustrosGalery/M4Galery.png"
 
 import s from "./MGalery.module.css"
@@ -12,7 +12,6 @@ function MGalery(){
         <div>
             <img className={s.M1Galery} src={M1Galery}></img>
             <img className={s.M4Galery} src={M4Galery}></img>
-            <img className={s.M3Galery} src={M3Galery}></img>
 
 
         </div>
